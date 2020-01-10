f9e2703d0c7aa037a746f8800b351867cd5c9644
Maxim Lyapin <maximl@mm.st>
2020-01-11 06:19:08 +1100

    Add simple memory arena implementation

---
+++ src/allocators/arena.c
@@ -0,0 +1,52 @@
+/* Copyright (c) 2019 Maxim Lyapin 
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *  of this software and associated documentation files the (Software""), to deal 
+ *  in the Software without restriction, including without limitation the rights 
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ *  copies of the Software, and to permit persons to whom the Software is 
+ *  furnished to do so, subject to the following conditions: 
+ *   
+ *  The above copyright notice and this permission notice shall be included in all 
+ *  copies or substantial portions of the Software. 
+ *   
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *  SOFTWARE.))
+ */
+
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+
+#include "arena.h"
+
+void arena_init(arena *a, void *mem, size_t mem_size)
+{
+	assert(a);
+	assert(mem);
+
+	memset(mem, 0, mem_size);
+	a->current_pos = mem;
+	a->arena_edge = mem + mem_size;
+}
+
+void *arena_alloc(arena *a, size_t size, size_t alignment)
+{
+	assert(a);
+	// Check that alignment is a value of power of two.
+	assert((alignment & -alignment) == alignment);
+
+	void *aligned = (void *)(((uintptr_t)a->current_pos + alignment - 1) &
+			-alignment);
+	if (aligned + size > a->arena_edge) {
+		return NULL;
+	} else {
+		a->current_pos = aligned + size;
+		return aligned;
+	}
+}
---
+++ src/allocators/arena_tests.c
@@ -0,0 +1,90 @@
+/* Copyright (c) 2019 Maxim Lyapin 
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *  of this software and associated documentation files the (Software""), to deal 
+ *  in the Software without restriction, including without limitation the rights 
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ *  copies of the Software, and to permit persons to whom the Software is 
+ *  furnished to do so, subject to the following conditions: 
+ *   
+ *  The above copyright notice and this permission notice shall be included in all 
+ *  copies or substantial portions of the Software. 
+ *   
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *  SOFTWARE.))
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include "unity/src/unity.h"
+
+#include "arena.h"
+
+static void can_allocate_all_the_given_memory(void) {
+	const size_t set_size = 100;
+	const size_t arena_size = sizeof(int64_t) * set_size; 
+	void *mem = malloc(arena_size);
+	assert(mem);
+	arena a;
+	arena_init(&a, mem, arena_size);
+
+	for (int i = 0; i < set_size; i++) {
+		int64_t *mem = arena_alloc(&a, sizeof(int64_t), 8);
+		TEST_ASSERT_NOT_NULL(mem);
+		*mem = -1;
+	}
+
+	free(mem);
+}
+
+static void cant_allocate_more_than_was_given(void) {
+	const size_t set_size = 100;
+	const size_t arena_size = sizeof(int64_t) * set_size; 
+	void *mem = malloc(arena_size);
+	assert(mem);
+	arena a;
+	arena_init(&a, mem, arena_size);
+
+	for (int i = 0; i < set_size; i++) {
+		int64_t *mem = arena_alloc(&a, sizeof(int64_t), 8);
+		TEST_ASSERT_NOT_NULL(mem);
+		*mem = -1;
+	}
+	mem = arena_alloc(&a, sizeof(int64_t), 64);
+	TEST_ASSERT_NULL(mem);
+
+	free(mem);
+}
+
+static void allignment_is_respected(void) {
+	void *mem = malloc(4096);
+	assert(mem);
+	arena a;
+	arena_init(&a, mem, 4096);
+
+	int16_t *allocated = arena_alloc(&a, sizeof(int16_t), 64);
+	TEST_ASSERT_NOT_NULL(allocated);
+	*allocated = -1;
+	TEST_ASSERT((uintptr_t)allocated % 64 == 0);
+}
+
+void setUp() {
+}
+
+void tearDown() {
+}
+
+int main (void) {
+	UNITY_BEGIN();
+	RUN_TEST(can_allocate_all_the_given_memory);
+	RUN_TEST(cant_allocate_more_than_was_given);
+	RUN_TEST(allignment_is_respected);
+	UNITY_END();
+}
